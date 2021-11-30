64e8ed9f627b9796c69c8f35c9e860396a068170
Kali <wellssam100@gmail.com>
2021-11-30 13:30:03 -0500

    Modified the bin2pcap script to take in command line args

--- main.py
+++ main.py
@@ -5,22 +5,10 @@
 import PIL.Image as Image
 import numpy
 import io
-import base64
 import cv2
-import os
-from pathlib import Path
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    #Read the binary information from the binary file.
-    data = Path('C:\\Users\\minhy\\PycharmProjects\\imageGen\\binary.txt').read_bytes()
-    print(data)
-
+def bin_to_png(inputfile, outputfile):
+    data = inputfile.read_bytes()
     arr = []
 
     for num in range(1200):
@@ -37,10 +25,29 @@ if __name__ == '__main__':
 
     # Convert the array to make a 400x300 grayscale image.
     grayImage = flatNumpyArray.reshape(30, 40)
-    cv2.imwrite('RandomGray.png', grayImage)
+    cv2.imwrite(outputfile, grayImage)
 
     # Convert the array to make a 400x100 color image.
     # bgrImage = flatNumpyArray.reshape(10, 40, 3)
     # cv2.imwrite('RandomColor.png', bgrImage)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main(argv):
+    inputfile=''
+    outputfile=''
+    try:
+        opts, args = getopt.getopt(argv, "hi:o:", ["ifile=", "ofile="])
+    except getopt.GetoptError:
+        print("bin2png.py -i inputfile -o outputfile")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print("bin2png.py -i <inputfile> -o <outputfile>")
+            sys.exit()
+        elif opt in ("-i","--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    bin_to_png(inputfile, outputfile)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
