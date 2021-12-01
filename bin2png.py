405d71477d41e8bde07f77a377d19e317ce480e0
minhyukko <57718746+minhyukko@users.noreply.github.com>
2021-11-30 22:05:18 -0500

    Squared the image

--- bin2png.py
+++ bin2png.py
@@ -6,17 +6,31 @@ import PIL.Image as Image
 import numpy
 import io
 import cv2
+import sys
+import getopt
+import math
+from pathlib import Path
 
 def bin_to_png(inputfile, outputfile):
+    inputfile = Path(inputfile)
+
     data = inputfile.read_bytes()
     arr = []
 
-    for num in range(1200):
+    for num in range(len(data)):
+        print(len(data))
         if data[num] == 49:
             arr.append(255)
         else:
             arr.append(0)
         # arr.append(data[num])
+
+    # Calculates the minimum length of square to fit all data
+    arrayLen = math.sqrt(len(data))
+    arrayLen = int(math.ceil(arrayLen))
+
+    for i in range(arrayLen * arrayLen - len(data)): # Pads the remaining space with white space to make the image square
+        arr.append(0)
 
     # Make an array of 1,200 random bytes.
     # randomByteArray = bytearray(os.urandom(1200))
@@ -24,7 +38,8 @@ def bin_to_png(inputfile, outputfile):
     flatNumpyArray = numpy.array(arr)
 
     # Convert the array to make a 400x300 grayscale image.
-    grayImage = flatNumpyArray.reshape(30, 40)
+
+    grayImage = flatNumpyArray.reshape(arrayLen, arrayLen)
     cv2.imwrite(outputfile, grayImage)
 
     # Convert the array to make a 400x100 color image.
@@ -39,7 +54,7 @@ def main(argv):
     except getopt.GetoptError:
         print("bin2png.py -i inputfile -o outputfile")
         sys.exit(2)
-    for opt, arg in opts:
+    for opt, arg in opts: #Not sure what your intension was here but I made a quick fix below
         if opt == "-h":
             print("bin2png.py -i <inputfile> -o <outputfile>")
             sys.exit()
@@ -47,6 +62,11 @@ def main(argv):
             inputfile = arg
         elif opt in ("-o", "--ofile"):
             outputfile = arg
+
+    # Quick fix
+    inputfile = argv[0]
+    outputfile = argv[1]
+
     bin_to_png(inputfile, outputfile)
 
 if __name__ == '__main__':
