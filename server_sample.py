e872b16a87df6863708aa73723bbe9d6a6af0bb9
JohnK-GMU <78662188+JohnK-GMU@users.noreply.github.com>
2022-03-15 14:32:07 -0400

    Update server_sample.py

--- server_sample.py
+++ server_sample.py
@@ -2,6 +2,17 @@ import sys
 import numpy as np
 import socket
 import time
+import threading
+#threads = []
+
+def AE(array):
+    print("In AE")
+    print("Data:{}".format(array))
+    print("Type: ", type(array))
+
+
+    #Min's Function Call
+    return
 
 def main(argv):
     HOST = "127.0.0.1"
@@ -17,17 +28,20 @@ def main(argv):
             curr_time=time.perf_counter()
             data,addr = s.recvfrom(7137)
             print("Connected by {}".format(addr))
-            data2 = np.frombuffer(data,dtype='S32', count=-1)
+            
             recv_time = time.perf_counter()
-            print("Data:{}".format(data2))
+            #print("Data:{}".format(data2))
+            #print("Type: ", type(data2))
             if not data:
                 print("Data Not Received\nClosing...\n")
                 conn.sendall(bytes(2))
                 break
 
-                
+            data2 = np.frombuffer(data,dtype='S32', count=-1)
             print("Received Data:{} s".format(recv_time-curr_time))
-
+            th = threading.Thread(target=AE, args=(data2,))
+            #AE(data2)
+            th.start()
             #conn.sendall(bytes(1))
     return
 
