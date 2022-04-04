a007b3266227c0a3adfbcea457b1660dce4131ff
minhyukko <57718746+minhyukko@users.noreply.github.com>
2022-04-03 22:03:42 -0400

    Changed the design and compatible to AF_UNIX

--- ufw_client.py
+++ ufw_client.py
@@ -13,24 +13,35 @@ def check(Ip):
     # and the string in search() method
     if(re.search(regex, Ip)):
         print("Valid Ip address")
-         
+        return True     
     else:
         print("Invalid Ip address")
-        exit()
+        return False
 
+s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+server_address = './uds_socket'
 try:
-    s.connect((socket.gethostname(), 1492))
+    s.connect(server_address)
+    #s.connect((socket.gethostname(), 1492))
+    #conn, add = s.connect((socket.gethostname(), 1492))
 except socket.error as err:
     print("Socket creation failed %s" % (err))
 
-
-
+#s.bind((socket.gethostname(), 1492))
+#s.listen()
+#conn, addr = s.accept()
 full_msg = ''
 while True:
-    msg = s.recv(15)
+    #s.connect((socket.gethostname(), 1492))
+    message = ('192.168.13.14')
+    encoded = message.encode('utf-8')
+    s.sendall(encoded)
+    msg = s.recv(1024)
+    #msg = conn.recv(1024)
+    print("Here: ")
+    #print(msg.decode())
 
     if len(msg) <= 0:
         break
@@ -41,13 +52,15 @@ while True:
             print("Socket creation failed %s" % (err))
     full_msg += msg.decode("utf-8")
 
-    check(full_msg)
-    
+    valid = check(full_msg)
+    if not valid:
+        continue
+
     x = '{ "actions":"b", "target_type":"sip", "target":"192.168.13.15"}'
     y = json.loads(x)
 
     print(full_msg)
     print(y["target"])
     # os.system(full_msg) #sudo ufw deny from 203.0.113.100
-    os.system("cal") #sudo ufw deny from 203.0.113.100
+    os.system("lscpu") #sudo ufw deny from 203.0.113.100
 
