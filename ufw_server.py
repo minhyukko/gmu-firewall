4dc09b3b0eebf2f470d1ac0a5a63396c210e52d1
minhyukko <57718746+minhyukko@users.noreply.github.com>
2022-04-03 22:02:58 -0400

    Changed the design and compatible to AF_UNIX

--- ufw_server.py
+++ ufw_server.py
@@ -1,18 +1,35 @@
 import socket
+import os 
 
 HOST = "127.0.0.1"
 PORT = 1492 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if os.path.exists("/tmp/socket_test.s"):
+    os.remove("/tmp/socket_test.s")
+
+server_address = './uds_socket'
+
+try:
+    os.unlink(server_address)
+except OSError:
+    if os.path.exists(server_address):
+        raise
+
+s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+s.bind(server_address)
 #s.bind((socket.gethostname(), 1235))
-s.bind((socket.gethostname(), 1492))
+#s.bind((socket.gethostname(), 1492))
 s.listen(15)
 
 while True:
     # now our endpoint knows about the OTHER endpoint.
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established.")
-
+    data = clientsocket.recv(1024)
+    if not data:
+        break
+    clientsocket.sendall(data)
+"""
     try:
         clientsocket.send(bytes("192.164.3.12","utf-8"))
     except clientsocket.error as err:
@@ -20,3 +37,4 @@ while True:
         clientsocket.connect((HOST, PORT))
 
     clientsocket.close()
+"""
