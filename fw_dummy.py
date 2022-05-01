c8fa9dc3ca054f05bf43fcf9516108f53170a582
meyassu <51754935+meyassu@users.noreply.github.com>
2022-05-01 01:14:07 -0400

    fixed socket descriptor references
    Socket descriptors were being referenced in inappropriate ways. These changes are an attempt to reference the appropriate socket descriptor whenever connections are being established.

--- fw_dummy.py
+++ fw_dummy.py
@@ -5,8 +5,8 @@ import json
 import signal
 import logging
 
-HOST = "127.0.0.1"
-PORT = 4913
+ae_address = "socket_fd/ae_fe.fd"
+
 regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
 
 def check_ip(Ip):
@@ -38,24 +38,20 @@ def terminate_connection(s, conn=None, err=False):
 
     quit()
 
-def setup_listener():
+def setup_listener(s):
     """
     Sets up a listening socket
     """
-    server_address = './uds_socket'
 
     try:
-        os.unlink(server_address)
+        os.unlink(ae_address)
     except OSError:
-        if os.path.exists(server_address):
+        if os.path.exists(ae_address):
             raise
 
-    log("setting up listener on " + str(HOST) + ", " + str(PORT) + "...", do_print=True)
-    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    s.bind(server_address)
-    s.listen()
-
-    return s
+    log("setting up listener on " + ae_address, do_print=True)
+    s.bind(ae_address)
+    s.listen(1)
 
 def implement_rule(msg):
     """
@@ -98,47 +94,48 @@ def main():
     fw_log =  './logs/fw.log'
     setup_logging(fw_log)
    
-    s = setup_listener()
-    # establish connection with AE (blocking call)
-    delim = "}"
-    conn, addr = s.accept()
-    log("accepted connection to AE @ " + str(addr), do_print=True)
-    with conn:
-        msg_i = 0
-        m_nxt = ""
-        while True:
-            get_meta = True
-            m = ""
-            msg = m_nxt
-            swoops = 0
-            # enter metadata / message processing cycle with NE 
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as ae_sock:
+        setup_listener(ae_sock)
+        # establish connection with AE (blocking call)
+        delim = "}"
+        conn, addr = ae_sock.accept()
+        log("accepted connection to AE @ " + str(addr), do_print=True)
+        with conn:
+            msg_i = 0
+            m_nxt = ""
             while True:
-                if swoops == 0: log("attempting to receieve message " + str(msg_i) + "...", do_print=True)
-                log("swoop " + str(swoops) + "...", do_print=True); swoops += 1
-                # get message segment
-                m = conn.recv(1024)
-                # logging.info("segment of message receieved:\n{m}")
-                if not m:
-                    terminate_connection(s, conn, err=True)
-                m = m.decode()
-                m_curr = m
-                # full message receieved
-                if delim in m:
-                    log("complete message received!", do_print=True)
-                    delim_idx = m.find(delim)
-                    # m may include parts of the next message
-                    m_curr = m[:delim_idx+1]
-                    m_nxt = ""
-                    if delim_idx < len(m) - 1:
-                        m_nxt = m[delim_idx+1:]
+                get_meta = True
+                m = ""
+                msg = m_nxt
+                swoops = 0
+                # enter metadata / message processing cycle with NE 
+                while True:
+                    if swoops == 0: log("attempting to receieve message " + str(msg_i) + "...", do_print=True)
+                    log("swoop " + str(swoops) + "...", do_print=True); swoops += 1
+                    # get message segment
+                    m = conn.recv(1024)
+                    # logging.info("segment of message receieved:\n{m}")
+                    if not m:
+                        terminate_connection(s, conn, err=True)
+                    m = m.decode()
+                    m_curr = m
+                    # full message receieved
+                    if delim in m:
+                        log("complete message received!", do_print=True)
+                        delim_idx = m.find(delim)
+                        # m may include parts of the next message
+                        m_curr = m[:delim_idx+1]
+                        m_nxt = ""
+                        if delim_idx < len(m) - 1:
+                            m_nxt = m[delim_idx+1:]
+                        msg += m_curr
+                        msg = json.loads(msg)
+                        implement_rule(msg)
+                        log("sending confirmation signal, onto the next message!", do_print=True)
+                        conn.sendall(b"1")
+                        msg_i += 1
+                        break
                     msg += m_curr
-                    msg = json.loads(msg)
-                    implement_rule(msg)
-                    log("sending confirmation signal, onto the next message!", do_print=True)
-                    conn.sendall(b"1")
-                    msg_i += 1
-                    break
-                msg += m_curr
 
 if __name__ == "__main__":
     main()
